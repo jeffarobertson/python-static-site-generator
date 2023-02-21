d9ed49f3ca248d1547daebb242e2bdb910458e8d
jeffrobertson <jrobertson@motivhealth.com>
2023-02-21 11:44:44 -0700

    add class Content

---
+++ ssg/content.py
@@ -0,0 +1,13 @@
+import re
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
+
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        cls.load(fm, Loader=FullLoader)
+        return cls(metadata, content)
