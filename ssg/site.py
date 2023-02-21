8f5fc2b48964922b0aef833f4523ec9d1a9cc881
jeffrobertson <jrobertson@motivhealth.com>
2023-02-21 13:21:20 -0700

    add error() method and update run_parser() error handling

--- ssg/site.py
+++ ssg/site.py
@@ -1,3 +1,5 @@
+import sys
+
 from pathlib import Path
 
 
@@ -28,4 +30,8 @@ class Site:
         parser = self.load_parser(path.suffix)
         if parser is not None:
             parser.parse(path, self.source, self.dest)
-        else: print("Not Implemented")
+        else: self.error("No parser for the {} extension, file skipped!".format(path.suffix))
+
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
