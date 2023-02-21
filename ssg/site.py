ec4c52965ddf2b1db26eebbf51110cb58e248785
jeffrobertson <jrobertson@motivhealth.com>
2023-02-21 09:57:37 -0700

    updating site.py and ssg.py

--- ssg/site.py
+++ ssg/site.py
@@ -6,4 +6,12 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        pass
+        directory = self.source / path.relative_to(self.source)
+        directory.mkdir(parents=True, exists_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if isinstance(path, dir):
+                Site.create_dir(path)
+    
