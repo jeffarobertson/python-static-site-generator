e908241a84dd207cc0ba5f2097ed17789c74b583
jeffrobertson <jrobertson@motivhealth.com>
2023-02-21 10:42:45 -0700

    Add write() method

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -11,6 +11,9 @@ class Parser:
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
     
-    def read(self, path: Path):
+    def read(self, path):
         with open(path, "r") as file:
             return file.read()
+        
+    def write(self, path, dest, content, ext=".html"):
+        full_path = dest / path.with_suffix(ext).name
