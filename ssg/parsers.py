b51f4e7d8d159cfd6cab90d13eb27ce33964306b
jeffrobertson <jrobertson@motivhealth.com>
2023-02-21 13:03:27 -0700

    renamed subclass ReStructuredParse to RestructuredTextParser

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -44,6 +44,5 @@ class MarkdownParser(Parser):
          self.write(path, dest, html)
          sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
-class ReStructuredParser(Parser):
+class ReStructuredTextParser(Parser):
     extensions = [".rst"]
-    
